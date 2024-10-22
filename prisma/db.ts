20ebadc7c60c6616cd723e6c0b9f1298f7b3e86e
danish mushtaq <suredanish@gmail.com>
2024-10-22 19:01:51 +0530

    refactor: db client inceptors deleted

--- prisma/db.ts
+++ prisma/db.ts
@@ -3,28 +3,10 @@ import { PrismaClient } from "@prisma/client";
 export default new PrismaClient().$extends({
     query: {
         async $allOperations({ operation, model, args, query }) {
-            try{
-            args.data && Object.keys(args.data).forEach(k => {
-                if (k.includes("List") || k.includes("Json") ){
-                    console.log(1111)
-                    args.data[k] = JSON.stringify(args.data[k]);
-                }
-            })
-            let result = await query(args)
-            result && Object.keys(result).forEach(k => {
-                if (k.includes("List") || k.includes("Json")){
-                    console.log(1111, k)
-                    result[k] = JSON.parse(result[k]);
-                    console.log(JSON.parse(result[k]))
-                }
-                if(k.includes("NumList")) result[k].map((i: String) => Number(i));
-            });
+            console.log(query)
+            const result = await query(args)
+            console.log(result)
             return result
-        }
-        catch(e)
-        {
-            console.error(e, args)
-        }
         },
     },
 })
